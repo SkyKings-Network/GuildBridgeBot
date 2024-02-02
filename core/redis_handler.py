7548785cd1298e2dd8fe7f76be09657c7bee8058
plun1331 <plun1331@gmail.com>
2024-02-01 19:03:28 -0800

    h

--- core/redis_handler.py
+++ core/redis_handler.py
@@ -115,17 +115,19 @@ class RedisManager:
             returned = await asyncio.wait(
                 [
                     asyncio.Task(self.bot.wait_for(
-                        "hypixel_guild_member_invite_failed", timeout=10,
+                        "hypixel_guild_member_invite_failed",
                         check=chk,
                     ), name="invite_failed"),
                     asyncio.Task(self.bot.wait_for(
-                        "hypixel_guild_member_invite", timeout=10,
+                        "hypixel_guild_member_invite",
                         check=chk,
                     ), name="invite_success")
                 ],
                 return_when=asyncio.FIRST_COMPLETED,
                 timeout=10,
             )
+            for task in returned[1]:
+                task.cancel()
             if len(returned[0]) == 0:
                 return {"success": False, "error": "timeout"}
             if returned[0].pop().get_name() == "invite_success":
