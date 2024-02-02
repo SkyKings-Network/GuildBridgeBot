0fa4ab5a93657200dc74525e388e3070cb18d6c2
plun1331 <plun1331@gmail.com>
2024-02-01 17:41:36 -0800

    fix: wrong dir

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ bot = DiscordBridgeBot()
 
 async def main():
     async with bot:
-        await bot.load_extension("commands.admin")
-        await bot.load_extension("commands.bridge")
-        await bot.load_extension("commands.generic")
+        await bot.load_extension("discord_extensions.admin")
+        await bot.load_extension("discord_extensions.bridge")
+        await bot.load_extension("discord_extensions.generic")
         await bot.start(config.token)
 
 
