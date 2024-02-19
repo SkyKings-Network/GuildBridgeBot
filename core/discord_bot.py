90c8047b1fda21124be0f275e9471bcdd7cc9fdb
plun1331 <plun1331@gmail.com>
2024-02-19 12:36:19 -0800

    Update discord_bot.py

--- core/discord_bot.py
+++ core/discord_bot.py
@@ -35,7 +35,7 @@ class DiscordBridgeBot(commands.Bot):
     async def send_invite(self, username):
         fut = asyncio.Future()
         self.invite_queue.put_nowait([username, fut])
-        if self._proc_invite_task is None or self._proc_invite_task.done():
+        if self._proc_inv_task is None or self._proc_inv_task.done():
             self._proc_inv_task = asyncio.create_task(self._process_invites())
         return await fut
 
