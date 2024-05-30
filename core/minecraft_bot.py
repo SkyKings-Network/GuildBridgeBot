84550eda81307e3f7b686a80ebba0b1a3f8b93d6
Jacktheguy <jackjjingle@gmail.com>
2024-05-29 22:02:05 -0400

    refactor: improve logging in MinecraftBotManager

--- core/minecraft_bot.py
+++ core/minecraft_bot.py
@@ -44,7 +44,7 @@ class MinecraftBotManager:
 
         @On(self.bot, "end")
         def kicked(this, reason):
-            print(f"Mineflayer > Bot offline: {reason}")
+            print(f"Mineflayer > Bot offline: {this} {reason}")
             self.send_to_discord("Bot Offline")
             self._online = False
             if self.auto_restart:
