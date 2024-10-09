fc58f01f85d6c589e75b5f373a09a7f0c6f2188e
HilFing <81067543+hilfing@users.noreply.github.com>
2024-10-09 01:31:15 +0000

    Fix#2

--- core/discord_bot.py
+++ core/discord_bot.py
@@ -760,7 +760,7 @@ class DiscordBridgeBot(commands.Bot):
                     return
                 parser = GuildMessageParser(message)
                 embeds = parser.parse()
-                if embeds:
+                if isinstance(embeds, list):
                     await self.send_debug_message("Sending guild command response")
                     for embed in embeds:
                         await self.send_message(embed=embed)
