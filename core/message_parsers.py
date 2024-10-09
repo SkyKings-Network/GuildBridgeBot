c16eae9ce345e66b99728b0d6c6b1bb8566e6bdd
HilFing <81067543+hilfing@users.noreply.github.com>
2024-10-09 02:05:59 +0000

    DEBUG#2

--- core/message_parsers.py
+++ core/message_parsers.py
@@ -55,13 +55,17 @@ class GuildMessageParser:
     def parse(self) -> str:
         # Determine message type and parse accordingly
         if "Top Guild Experience" in self.raw_message:
+            print("Top")
             return self._parse_top_message()
         elif "Total Members:" in self.raw_message:
             if "Offline Members:" in self.raw_message:
+                print("Online")
                 return self._parse_online_message()
             else:
+                print("List")
                 return self._parse_list_message()
         elif "Created:" in self.raw_message:
+            print("Info")
             return self._create_guild_stats_embed()
         else:
             return "NaN"
