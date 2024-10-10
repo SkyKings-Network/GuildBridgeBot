a08b1d5d93e6bede241fe126e07c44112daffd72
Jacktheguy <jackjjingle@gmail.com>
2024-10-09 23:22:15 -0400

    .

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from core.discord_bot import DiscordBridgeBot
 import asyncio
-from core.config import DiscordConfig, SettingsConfig
 import os
+import subprocess
+import sys
+
+try:
+    from core.discord_bot import DiscordBridgeBot
+    from core.config import DiscordConfig, SettingsConfig
+except ModuleNotFoundError as e:
+    missing_module = str(e).split("'")[1]
+    print(f"Module {missing_module} not found. Installing...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", missing_module])
+    print(f"Module {missing_module} installed. Restarting bot...")
+    os.execv(sys.executable, ['python'] + sys.argv)
 
 bot = DiscordBridgeBot()
 
@@ -17,6 +27,5 @@ async def main():
                 print(f"Ext > {extension} loaded!")
             print("Ext > Extensions loaded!")
         await bot.start(DiscordConfig.token)
-
 
 asyncio.run(main())
